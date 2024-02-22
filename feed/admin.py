95906a49e803050004a8070ded028323d2216adb
Artem Novok <aan200545@gmail.com>
2024-02-21 16:20:26 -0800

    Registered model

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmit(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmit)
--- feed/models.py
+++ feed/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Post (models.Model):
     text = models.CharField(max_length=240)
+    
+    
+    def __str__(self) -> str:
+        return self.text
