94ad94808a1fb4cec7d00f0cc185993ba5ec632e
Artem Novok <aan200545@gmail.com>
2024-02-22 16:21:03 -0800

    Added receiver

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -11,3 +12,8 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
     
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create a new UserProfile() obj when new Django user is created """
+    if created:
+        new_user = UserProfile.objects.create(user=instance)
