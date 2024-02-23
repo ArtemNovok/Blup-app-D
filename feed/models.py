45847a154e0f8ca3b1f6019fb8d39c62a8653588
Artem Novok <aan200545@gmail.com>
2024-02-22 17:15:07 -0800

    Added images to post's author

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
 class Post (models.Model):
     title = models.CharField(max_length = 35, blank = False, null=False)
     text = models.CharField(max_length=240, blank = False, null=False)
     date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     
     
     def __str__(self) -> str:
