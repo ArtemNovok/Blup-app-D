79159ca98e3e64fe16ac60c821c2e8409cc7c9fe
Artem Novok <aan200545@gmail.com>
2024-02-23 16:39:14 -0800

    Added post detail view

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = "feed"
 
 urlpatterns = [
-    path("", views.HomePage.as_view(), name="index")
+    path("", views.HomePage.as_view(), name="index"),
+    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail')
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView
+from django.views.generic import DetailView
 # Create your views here.
 
 class HomePage(ListView):
@@ -9,3 +10,9 @@ class HomePage(ListView):
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by("-id")[:30]
+
+class DetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = 'feed/detail.html'
+    model = Post
+    context_object_name = 'post'
