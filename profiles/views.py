625c77dcc4088588c2f12ea57f6f22e7f2152c5a
Artem Novok <aan200545@gmail.com>
2024-02-25 18:10:01 -0800

    Added profile details

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,18 @@
+from typing import Any
+from feed.models import Post
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.views.generic import DetailView
 # Create your views here.
+class ProfileView(DetailView):
+    http_method_names=['get']
+    template_name='profiles/profile.html'
+    model = User
+    context_object_name = 'user'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        user = self.get_object()
+        context=super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author = user).count()
+        return context
--- til/urls.py
+++ til/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path 
-from feed import urls as feed_urls
 from django.conf import settings
+from feed import urls as feed_urls
 from django.conf.urls.static import static
+from django.urls import path, include, re_path 
+from profiles import urls as profiles_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
-    path("", include('allauth.urls'))
+    path("", include('allauth.urls')),
+    path("profile/", include(profiles_urls, namespace="profiles"))
  ]  + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
